1a118ed63762dfd76bf9f7d192284dd46dd48422
Vic Amuso <vic@parablesoft.com>
2018-01-23 15:58:48 -0500

    Converted to yarn and fixed the issue with isDestroying

--- addon/components/action-notice.js
+++ addon/components/action-notice.js
@@ -3,15 +3,33 @@ import layout from '../templates/components/action-notice';
 
 const{observer,Component,get,set} = Ember;
 
-export default Ember.Component.extend({
+export default Component.extend({
   layout,
   tagName: "",
   interval: 2500,
+  hideIntervalId: null,
   hideNotice: observer("show",function(){
+    if(this.isDestroyed)
+      return;
+
     let {show,interval} = this.getProperties("show","interval");
     if(!show){return;}
-    Ember.run.later(this,function(){
+    let hideIntervalId = Ember.run.later(this,function(){
       set(this,"show",false);
+      set(this,"hideIntervalId",null);
     },interval);
+    set(this,"hideIntervalId",hideIntervalId);
   }),
+  willDestroyElement(){
+    this._super(...arguments);
+    if(this.isDestroyed)
+      return;
+
+    let hideIntervalId = get(this,"hideIntervalId");
+    if(hideIntervalId){
+      set(this,"show",false);
+      Ember.run.cancel(hideIntervalId);
+    }
+  }
+
 });
